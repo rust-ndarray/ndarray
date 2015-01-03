a434ae5762bec527a389952a0d78a103fb9e9ad1
root <root@localhost>
2015-01-03 14:22:06 +0100

    Update RawPtr -> PtrExt

--- src/dimension.rs
+++ src/dimension.rs
@@ -195,7 +195,7 @@ fn abs_index(len: Ixs, index: Ixs) -> Ix {
 ///
 /// **Panics** if `index` is larger than the size of the axis
 // FIXME: Move to Dimension trait
-pub fn do_sub<A, D: Dimension, P: Copy + RawPtr<A>>(dims: &mut D, ptr: &mut P, strides: &D,
+pub fn do_sub<A, D: Dimension, P: Copy + PtrExt<A>>(dims: &mut D, ptr: &mut P, strides: &D,
                            axis: uint, index: Ix)
 {
     let dim = dims.slice()[axis];
