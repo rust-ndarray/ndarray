43d571e5403a1a654eb6d1e8cbdee073bb02a162
root <root@localhost>
2015-01-18 18:17:57 +0100

    Fix compilation of benches after black_box change.

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -29,7 +29,7 @@ fn bench_std_iter_1d(bench: &mut test::Bencher)
                          3., 4., 4.,
                          3., 4., 4.,
                          5., 6., 6.]);
-    bench.iter(|| for &elt in a.iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.iter() { black_box(elt); })
 }
 
 #[bench]
@@ -40,7 +40,7 @@ fn bench_std_iter_1d_raw(bench: &mut test::Bencher)
                          3., 4., 4.,
                          3., 4., 4.,
                          5., 6., 6.]);
-    bench.iter(|| for &elt in a.raw_data().iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.raw_data().iter() { black_box(elt); })
 }
 
 #[bench]
@@ -51,28 +51,28 @@ fn bench_std_iter_2d(bench: &mut test::Bencher)
                           [3., 4., 4.],
                           [3., 4., 4.],
                           [5., 6., 6.]]);
-    bench.iter(|| for &elt in a.iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.iter() { black_box(elt); })
 }
 
 #[bench]
 fn bench_std_iter_1d_large(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((1024));
-    bench.iter(|| for &elt in a.iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.iter() { black_box(elt); })
 }
 
 #[bench]
 fn bench_std_iter_1d_raw_large(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((1024));
-    bench.iter(|| for &elt in a.raw_data().iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.raw_data().iter() { black_box(elt); })
 }
 
 #[bench]
 fn bench_std_iter_2d_large(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((16, 64));
-    bench.iter(|| for &elt in a.iter() { black_box(elt) })
+    bench.iter(|| for &elt in a.iter() { black_box(elt); })
 }
 
 #[bench]
@@ -91,7 +91,7 @@ fn assign_scalar_2d(bench: &mut test::Bencher)
 fn bench_iter_diag(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((1024, 1024));
-    bench.iter(|| for elt in a.diag_iter() { black_box(elt) })
+    bench.iter(|| for elt in a.diag_iter() { black_box(elt); })
 }
 
 #[bench]
@@ -99,14 +99,14 @@ fn bench_row_iter(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((1024, 1024));
     let it = a.row_iter(17);
-    bench.iter(|| for elt in it.clone() { black_box(elt) })
+    bench.iter(|| for elt in it.clone() { black_box(elt); })
 }
 
 #[bench]
 fn bench_col_iter(bench: &mut test::Bencher)
 {
     let a = Array::<f32, _>::zeros((1024, 1024));
-    bench.iter(|| for elt in a.col_iter(17) { black_box(elt) })
+    bench.iter(|| for elt in a.col_iter(17) { black_box(elt); })
 }
 
 #[bench]
