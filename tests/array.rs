c5dff8945c526b6cdbae84938a8cc98726ce7ed6
bluss <bluss@users.noreply.github.com>
2015-12-19 23:12:35 +0100

    Move char array test to tests/tests.rs

--- tests/array.rs
+++ tests/array.rs
@@ -544,3 +544,11 @@ fn arithmetic_broadcast() {
     let b = a.clone() / aview0(&1.);
     assert_eq!(a, b);
 }
+
+#[test]
+fn char_array()
+{
+    // test compilation & basics of non-numerical array
+    let cc = Array::from_iter("alphabet".chars()).reshape((4, 2));
+    assert!(cc.subview(1, 0) == Array::from_iter("apae".chars()));
+}
