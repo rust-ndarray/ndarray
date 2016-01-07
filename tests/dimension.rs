b6b6f4840b3cf443061b85c2e3dc7a30af5d690a
bluss <bluss@users.noreply.github.com>
2016-01-07 15:24:56 +0100

    Change .subview() to return an ArrayView and add .into_subview()

--- tests/dimension.rs
+++ tests/dimension.rs
@@ -17,7 +17,7 @@ fn remove_axis()
     assert_eq!(vec![4, 5, 6].remove_axis(1), vec![4, 6]);
 
     let a = Array::<f32, _>::zeros(vec![4,5,6]);
-    let _b = a.subview(1, 0).reshape((4, 6)).reshape(vec![2, 3, 4]);
+    let _b = a.into_subview(1, 0).reshape((4, 6)).reshape(vec![2, 3, 4]);
     
 }
 
