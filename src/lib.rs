2853d5432b0d3b225c94c38b28731f52a5a11bdf
Vincent Barrielle <vincent.barrielle@dynamixyz.com>
2016-01-21 14:24:58 +0100

    add strided constructor for ArrayView

--- src/lib.rs
+++ src/lib.rs
@@ -851,6 +851,45 @@ impl<'a, A, D> ArrayViewMut<'a, A, D>
         }
     }
 
+    /// Create an `ArrayView` borrowing its data from a slice.
+    ///
+    /// Checks whether `dim` and `strides` are compatible with the slice's
+    /// length, returning an `Err` if not compatible.
+    ///
+    /// ```
+    /// use ndarray::ArrayViewMut;
+    /// use ndarray::arr3;
+    ///
+    /// let s = &mut [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    /// let mut a = ArrayViewMut::from_slice_dim_stride((2, 3, 2),
+    ///                                                 (1, 4, 2),
+    ///                                                 s).unwrap();
+    ///
+    /// a[[0, 0, 0]] = 1;
+    /// assert!(
+    ///     a == arr3(&[[[1, 2],
+    ///                  [4, 6],
+    ///                  [8, 10]],
+    ///                 [[1, 3],
+    ///                  [5, 7],
+    ///                  [9, 11]]])
+    /// );
+    /// assert!(a.strides() == &[1, 4, 2]);
+    /// ```
+    pub fn from_slice_dim_stride(dim: D,
+                                 strides: D,
+                                 s: &'a mut [A]
+                                 ) -> Result<Self, StrideError>
+    {
+        dimension::can_index_slice(s,
+                                   &dim,
+                                   &strides).map(|_| {
+            unsafe {
+                Self::new_(s.as_mut_ptr(), dim, strides)
+            }
+        })
+    }
+
     #[inline]
     fn into_base_iter(self) -> Baseiter<'a, A, D> {
         unsafe {
