b2f8d9e23913ad040ee6e26995f62a5fd2284268
Vincent Barrielle <vincent.barrielle@m4x.org>
2016-03-04 13:16:51 +0100

    stacking is now a free function `ndarray::stack`

--- src/stacking.rs
+++ src/stacking.rs
@@ -1,48 +1,34 @@
 
 use imp_prelude::*;
 
-/// A slice extension trait for concatenating arrays.
-pub trait ArrayStackingExt {
-    type Output;
-
-    /// Stack the given arrays along the specified axis.
-    ///
-    /// *Panics* if axis is out of bounds
-    /// *Panics* if the slice is empty.
-    fn stack(&self, axis: Axis) -> Self::Output;
-}
-
-impl<'a, A, D> ArrayStackingExt for [ArrayView<'a, A, D>]
+pub fn stack<'a, A, D>(arrays: &[ArrayView<'a, A, D>], axis: Axis)
+    -> OwnedArray<A, D>
     where A: Copy,
           D: Dimension + RemoveAxis
 {
-    type Output = OwnedArray<A, D>;
+    assert!(arrays.len() > 0);
+    let mut res_dim = arrays[0].dim().clone();
+    let stacked_dim = arrays.iter()
+                            .fold(0, |acc, a| acc + a.dim().index(axis));
+    *res_dim.index_mut(axis) = stacked_dim;
 
-    fn stack(&self, axis: Axis) -> <Self as ArrayStackingExt>::Output {
-        assert!(self.len() > 0);
-        let mut res_dim = self[0].dim().clone();
-        let stacked_dim = self.iter()
-                              .fold(0, |acc, a| acc + a.dim().index(axis));
-        *res_dim.index_mut(axis) = stacked_dim;
-
-        // we can safely use uninitialized values here because they are Copy
-        // and we will only ever write to them
-        let size = res_dim.size();
-        let mut v = Vec::with_capacity(size);
-        unsafe {
-            v.set_len(size);
-        }
-        let mut res = OwnedArray::from_vec_dim(res_dim, v).unwrap();
-
-        {
-            let mut assign_view = res.view_mut();
-            for array in self {
-                let len = *array.dim().index(axis);
-                let (mut front, rest) = assign_view.split_at(axis, len);
-                front.assign(array);
-                assign_view = rest;
-            }
-        }
-        res
+    // we can safely use uninitialized values here because they are Copy
+    // and we will only ever write to them
+    let size = res_dim.size();
+    let mut v = Vec::with_capacity(size);
+    unsafe {
+        v.set_len(size);
     }
+    let mut res = OwnedArray::from_vec_dim(res_dim, v).unwrap();
+
+    {
+        let mut assign_view = res.view_mut();
+        for array in arrays {
+            let len = *array.dim().index(axis);
+            let (mut front, rest) = assign_view.split_at(axis, len);
+            front.assign(array);
+            assign_view = rest;
+        }
+    }
+    res
 }
