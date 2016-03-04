3a3e27809f66634f5326256f21d428010a5a84b6
Vincent Barrielle <vincent.barrielle@m4x.org>
2016-03-04 21:20:32 +0100

    stacking returns OutOfBounds error instead of panicking

--- src/stacking.rs
+++ src/stacking.rs
@@ -3,17 +3,18 @@ use imp_prelude::*;
 use error::{ShapeError, ErrorKind, from_kind};
 
 /// Stack arrays along the given axis.
-///
-/// *Panics* if axis is out of bounds.
 pub fn stack<'a, A, D>(arrays: &[ArrayView<'a, A, D>], axis: Axis)
     -> Result<OwnedArray<A, D>, ShapeError>
     where A: Copy,
           D: Dimension + RemoveAxis
 {
+    let mut res_dim = arrays[0].dim().clone();
+    if axis.axis() >= res_dim.slice().len() {
+        return Err(from_kind(ErrorKind::OutOfBounds));
+    }
     if arrays.len() == 0 {
         return Err(from_kind(ErrorKind::Unsupported));
     }
-    let mut res_dim = arrays[0].dim().clone();
     let common_dim = res_dim.remove_axis(axis);
     if arrays.iter().any(|a| a.dim().remove_axis(axis) != common_dim) {
         return Err(from_kind(ErrorKind::IncompatibleShape));
