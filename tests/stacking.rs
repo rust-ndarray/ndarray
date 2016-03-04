2cef3d8853f338d1d9ca7f46251eae38cf9dfa3f
Vincent Barrielle <vincent.barrielle@m4x.org>
2016-03-04 13:28:25 +0100

    `stack` now returns `Result<OwnedArray, ShapeError>`

--- tests/stacking.rs
+++ tests/stacking.rs
@@ -11,7 +11,7 @@ use ndarray::{
 fn vstack() {
     let a = arr2(&[[2., 2.],
                    [3., 3.]]);
-    let b = ndarray::stack(&[a.view(), a.view()], Axis(0));
+    let b = ndarray::stack(&[a.view(), a.view()], Axis(0)).unwrap();
     assert_eq!(b, arr2(&[[2., 2.],
                          [3., 3.],
                          [2., 2.],
