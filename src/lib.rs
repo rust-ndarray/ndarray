fe3e01a16fa8667f37cc93181d1820887adee606
bluss <bluss@users.noreply.github.com>
2016-03-06 02:42:45 +0100

    Add license notice to source files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2014-2016 bluss and ndarray developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 #![crate_name="ndarray"]
 #![cfg_attr(has_deprecated, feature(deprecated))]
 #![doc(html_root_url = "http://bluss.github.io/rust-ndarray/master/")]
