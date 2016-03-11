c1985fcb13eccd3b9fc66c37c224de64aa0abd00
bluss <bluss@users.noreply.github.com>
2016-03-11 17:05:19 +0100

    Add shape & stride info to Debug for arrays

--- tests/format.rs
+++ tests/format.rs
@@ -7,25 +7,25 @@ use ndarray::{arr0, rcarr1, aview1};
 fn formatting()
 {
     let a = rcarr1::<f32>(&[1., 2., 3., 4.]);
-    assert_eq!(format!("{:?}", a),
+    assert_eq!(format!("{}", a),
                //"[   1,    2,    3,    4]");
                "[1, 2, 3, 4]");
-    assert_eq!(format!("{:4?}", a),
+    assert_eq!(format!("{:4}", a),
                "[   1,    2,    3,    4]");
     let a = a.reshape((4, 1, 1));
-    assert_eq!(format!("{:4?}", a),
+    assert_eq!(format!("{:4}", a),
                "[[[   1]],\n [[   2]],\n [[   3]],\n [[   4]]]");
 
     let a = a.reshape((2, 2));
     assert_eq!(format!("{}", a), 
                "[[1, 2],\n [3, 4]]");
-    assert_eq!(format!("{:?}", a), 
+    assert_eq!(format!("{}", a),
                "[[1, 2],\n [3, 4]]");
-    assert_eq!(format!("{:#4?}", a),
+    assert_eq!(format!("{:#4}", a),
                "[[   1,    2], [   3,    4]]");
 
     let b = arr0::<f32>(3.5);
-    assert_eq!(format!("{:?}", b),
+    assert_eq!(format!("{}", b),
                "3.5");
 
     let s = format!("{:.3e}", aview1::<f32>(&[1.1, 2.2, 33., 440.]));
