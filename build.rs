d95da302045767dfb186cc6e2f9da3a27925ace9
bluss <bluss@users.noreply.github.com>
2016-04-13 16:38:06 +0200

    Use deprecated where it's stable in 1.9 beta & nightly

--- build.rs
+++ build.rs
@@ -12,15 +12,15 @@ const DEPRECATED_CFG: &'static str = "has_deprecated";
 fn main() {
     let version = rustc_version::version_meta();
     println!("cargo:rerun-if-changed=build.rs");
-    if version.channel == Channel::Nightly {
+    if version.channel == Channel::Nightly || version.channel == Channel::Beta {
         if let Some(ref date) = version.commit_date { 
             // parse year, month, day
             let ndate = date.splitn(3, "-")
                                       .map(str::parse)
                                       .collect::<Result<Vec<i32>, _>>().unwrap();
 
-            // deprecated is available from nightly 2015-12-18
-            if ndate >= vec![2015, 12, 18] {
+            // deprecated is stable from nightly 04-12
+            if ndate >= vec![2016, 04, 12] {
                 println!("cargo:rustc-cfg={}", DEPRECATED_CFG);
             }
         }
