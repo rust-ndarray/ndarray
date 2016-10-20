da84c9b163a8ea0b2e6ecc991e4defe1fba1e8ae
bluss <bluss@users.noreply.github.com>
2016-10-20 16:45:30 +0200

    Have the type aliases for various sizes in the top level

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,7 @@ pub use stacking::stack;
 
 pub use shape_builder::{ ShapeBuilder };
 
+mod aliases;
 mod arraytraits;
 #[cfg(feature = "serde")]
 mod array_serde;
@@ -120,61 +121,7 @@ mod array_serialize;
 mod arrayformat;
 mod data_traits;
 
-/// Type aliases for common array sizes
-pub mod alias {
-    use ::{Ix, Array, ArrayView, ArrayViewMut};
-    /// zero-dimensionial
-    pub type Ix0 = ();
-    /// one-dimensional
-    pub type Ix1 = Ix;
-    /// two-dimensional
-    pub type Ix2 = (Ix, Ix);
-    /// three-dimensional
-    pub type Ix3 = (Ix, Ix, Ix);
-    /// four-dimensional
-    pub type Ix4 = (Ix, Ix, Ix, Ix);
-    /// dynamic-dimensional
-    pub type IxDyn = Vec<Ix>;
-
-    /// zero-dimensional array
-    pub type Array0<A> = Array<A, Ix0>;
-    /// one-dimensional array
-    pub type Array1<A> = Array<A, Ix1>;
-    /// two-dimensional array
-    pub type Array2<A> = Array<A, Ix2>;
-    /// three-dimensional array
-    pub type Array3<A> = Array<A, Ix3>;
-    /// four-dimensional array
-    pub type Array4<A> = Array<A, Ix4>;
-    /// dynamic-dimensional array
-    pub type ArrayD<A> = Array<A, IxDyn>;
-
-    /// zero-dimensional array view
-    pub type ArrayView0<'a, A> = ArrayView<'a, A, Ix0>;
-    /// one-dimensional array view
-    pub type ArrayView1<'a, A> = ArrayView<'a, A, Ix1>;
-    /// two-dimensional array view
-    pub type ArrayView2<'a, A> = ArrayView<'a, A, Ix2>;
-    /// three-dimensional array view
-    pub type ArrayView3<'a, A> = ArrayView<'a, A, Ix3>;
-    /// four-dimensional array view
-    pub type ArrayView4<'a, A> = ArrayView<'a, A, Ix4>;
-    /// dynamic-dimensional array view
-    pub type ArrayViewD<'a, A> = ArrayView<'a, A, IxDyn>;
-
-    /// zero-dimensional read-write array view
-    pub type ArrayViewMut0<'a, A> = ArrayViewMut<'a, A, Ix0>;
-    /// one-dimensional read-write array view
-    pub type ArrayViewMut1<'a, A> = ArrayViewMut<'a, A, Ix1>;
-    /// two-dimensional read-write array view
-    pub type ArrayViewMut2<'a, A> = ArrayViewMut<'a, A, Ix2>;
-    /// three-dimensional read-write array view
-    pub type ArrayViewMut3<'a, A> = ArrayViewMut<'a, A, Ix3>;
-    /// four-dimensional read-write array view
-    pub type ArrayViewMut4<'a, A> = ArrayViewMut<'a, A, Ix4>;
-    /// dynamic-dimensional read-write array view
-    pub type ArrayViewMutD<'a, A> = ArrayViewMut<'a, A, IxDyn>;
-}
+pub use aliases::*;
 
 pub use data_traits::{
     Data,
