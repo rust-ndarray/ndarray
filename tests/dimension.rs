62caab792d1c02e5c29dae428543f2ce2441f7b8
bluss <bluss@users.noreply.github.com>
2016-10-30 20:30:17 +0100

    Update tests/dimension.rs

--- tests/dimension.rs
+++ tests/dimension.rs
@@ -12,9 +12,9 @@ use ndarray::{
 #[test]
 fn remove_axis()
 {
-    assert_eq!(3.remove_axis(Axis(0)), ());
-    assert_eq!((1, 2).remove_axis(Axis(0)), 2);
-    assert_eq!((4, 5, 6).remove_axis(Axis(1)), (4, 6));
+    assert_eq!([3].remove_axis(Axis(0)), []);
+    assert_eq!([1, 2].remove_axis(Axis(0)), [2]);
+    assert_eq!([4, 5, 6].remove_axis(Axis(1)), [4, 6]);
 
     assert_eq!(vec![1,2].remove_axis(Axis(0)), vec![2]);
     assert_eq!(vec![4, 5, 6].remove_axis(Axis(1)), vec![4, 6]);
@@ -50,14 +50,14 @@ fn fastest_varying_order() {
     let order = strides._fastest_varying_stride_order();
     assert_eq!(order.slice(), &[3, 0, 2, 1]);
 
-    assert_eq!((1, 3)._fastest_varying_stride_order(), (0, 1));
-    assert_eq!((7, 2)._fastest_varying_stride_order(), (1, 0));
-    assert_eq!((6, 1, 3)._fastest_varying_stride_order(), (1, 2, 0));
+    assert_eq!([1, 3]._fastest_varying_stride_order(), [0, 1]);
+    assert_eq!([7, 2]._fastest_varying_stride_order(), [1, 0]);
+    assert_eq!([6, 1, 3]._fastest_varying_stride_order(), [1, 2, 0]);
 
     // it's important that it produces distinct indices. Prefer the stable order
     // where 0 is before 1 when they are equal.
-    assert_eq!((2, 2)._fastest_varying_stride_order(), (0, 1));
-    assert_eq!((2, 2, 1)._fastest_varying_stride_order(), (2, 0, 1));
-    assert_eq!((2, 2, 3, 1, 2)._fastest_varying_stride_order(), (3, 0, 1, 4, 2));
+    assert_eq!([2, 2]._fastest_varying_stride_order(), [0, 1]);
+    assert_eq!([2, 2, 1]._fastest_varying_stride_order(), [2, 0, 1]);
+    assert_eq!([2, 2, 3, 1, 2]._fastest_varying_stride_order(), [3, 0, 1, 4, 2]);
 }
 
