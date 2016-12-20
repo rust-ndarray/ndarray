8fe29ebface127b256d80b472286121ed2cf64a8
bluss <bluss@users.noreply.github.com>
2016-12-20 20:58:58 +0100

    Add cut & transposed case to fold

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -255,6 +255,18 @@ fn fold_sum_i32_2d_transpose(bench: &mut test::Bencher)
 }
 
 #[bench]
+fn fold_sum_i32_2d_cutout_transpose(bench: &mut test::Bencher)
+{
+    let a = Array::<i32, _>::zeros((66, 66));
+    let mut av = a.slice(s![1..-1, 1..-1]);
+    av.swap_axes(0, 1);
+    let a = black_box(av);
+    bench.iter(|| {
+        a.fold(0, |acc, &x| acc + x)
+    });
+}
+
+#[bench]
 fn add_2d_regular(bench: &mut test::Bencher)
 {
     let mut a = Array::<i32, _>::zeros((64, 64));
