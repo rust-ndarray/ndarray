17a1180f3ca34ae534e70a341f2ce4a5d8669649
bluss <bluss@users.noreply.github.com>
2016-12-21 21:38:13 +0100

    Update tests for parallel axis iterators

--- tests/rayon.rs
+++ tests/rayon.rs
@@ -7,22 +7,26 @@ use ndarray::prelude::*;
 
 use rayon::prelude::*;
 
+const M: usize = 1024 * 10;
+const N: usize = 100;
+
 #[test]
 fn test_axis_iter() {
-    let mut a = Array2::<u32>::zeros((1024 * 1024, 100));
+    let mut a = Array2::<f64>::zeros((M, N));
     for (i, mut v) in a.axis_iter_mut(Axis(0)).enumerate() {
         v.fill(i as _);
     }
-    assert_eq!(a.axis_iter(Axis(0)).len(), 1024 * 1024);
+    assert_eq!(a.axis_iter(Axis(0)).len(), M);
     let s = a.axis_iter(Axis(0)).into_par_iter().map(|x| x.scalar_sum()).sum();
+    println!("{:?}", a.slice(s![..10, ..5]));
     assert_eq!(s, a.scalar_sum());
 }
 
 #[test]
 fn test_axis_iter_mut() {
-    let mut a = Array2::<u32>::zeros((1024 * 1024, 100));
-    a.axis_iter_mut(Axis(0)).into_par_iter().enumerate().for_each(|(i, mut v)| v.fill(i as _));
-    assert_eq!(a.scalar_sum(),
-               (0..a.len_of(Axis(0))).map(|n| n as u32 * a.len_of(Axis(1)) as u32).sum::<u32>());
-    println!("{:?}", a.slice(s![..10, ..10]));
+    let mut a = Array::linspace(0., 1.0f64, M * N).into_shape((M, N)).unwrap();
+    let b = a.mapv(|x| x.exp());
+    a.axis_iter_mut(Axis(0)).into_par_iter().for_each(|mut v| v.mapv_inplace(|x| x.exp()));
+    println!("{:?}", a.slice(s![..10, ..5]));
+    assert!(a.all_close(&b, 0.001));
 }
