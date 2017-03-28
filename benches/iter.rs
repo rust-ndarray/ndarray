47588c6baf7a3ca6bae95f3c93e0bc02cb075182
bluss <bluss@users.noreply.github.com>
2017-03-28 23:02:19 +0200

    Add benchmarks comparing libstd zip to azip for vectors

--- benches/iter.rs
+++ benches/iter.rs
@@ -3,9 +3,10 @@
 extern crate test;
 use test::Bencher;
 
-#[macro_use(s)]
+#[macro_use(s, azip)]
 extern crate ndarray;
 use ndarray::prelude::*;
+use ndarray::{Zip, FoldWhile};
 
 #[bench]
 fn iter_sum_2d_regular(bench: &mut Bencher)
@@ -87,5 +88,61 @@ fn iter_filter_sum_2d_stride_f32(bench: &mut Bencher)
     let b = b.slice(s![.., ..;2]);
     bench.iter(|| {
         b.iter().filter(|&&x| x < 75.).fold(0., |acc, &x| acc + x)
+    });
+}
+
+const ZIPSZ: usize = 10_000;
+
+#[bench]
+fn sum_3_std_zip1(bench: &mut Bencher)
+{
+    let a = vec![1; ZIPSZ];
+    let b = vec![1; ZIPSZ];
+    let c = vec![1; ZIPSZ];
+    bench.iter(|| {
+        a.iter().zip(b.iter().zip(&c)).fold(0, |acc, (&a, (&b, &c))| {
+            acc + a + b + c
+        })
+    });
+}
+
+#[bench]
+fn sum_3_std_zip2(bench: &mut Bencher)
+{
+    let a = vec![1; ZIPSZ];
+    let b = vec![1; ZIPSZ];
+    let c = vec![1; ZIPSZ];
+    bench.iter(|| {
+        a.iter().zip(b.iter()).zip(&c).fold(0, |acc, ((&a, &b), &c)| {
+            acc + a + b + c
+        })
+    });
+}
+
+#[bench]
+fn sum_3_azip(bench: &mut Bencher)
+{
+    let a = vec![1; ZIPSZ];
+    let b = vec![1; ZIPSZ];
+    let c = vec![1; ZIPSZ];
+    bench.iter(|| {
+        let mut s = 0;
+        azip!(a, b, c in {
+            s += a + b + c;
+        });
+        s
+    });
+}
+
+#[bench]
+fn sum_3_azip_fold(bench: &mut Bencher)
+{
+    let a = vec![1; ZIPSZ];
+    let b = vec![1; ZIPSZ];
+    let c = vec![1; ZIPSZ];
+    bench.iter(|| {
+        Zip::from(&a).and(&b).and(&c).fold_while(0, |acc, &a, &b, &c| {
+            FoldWhile::Continue(acc + a + b + c)
+        }).into_inner()
     });
 }
