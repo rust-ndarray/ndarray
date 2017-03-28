b94bdd585b3e60e503749fcbc472773603e1d0ce
bluss <bluss@users.noreply.github.com>
2017-03-28 21:04:01 +0200

    zip: Update fastchunks example

--- examples/fastchunks.rs
+++ examples/fastchunks.rs
@@ -4,29 +4,28 @@ extern crate ndarray;
 
 use ndarray::prelude::*;
 use ndarray::Zip;
-use std::marker::PhantomData;
 
 fn main() {
-    let mut a = <Array1<f32>>::linspace(1., 100., 10 * 10).into_shape((10, 10)).unwrap();
+    let a = <Array1<f32>>::linspace(1., 100., 10 * 10).into_shape((10, 10)).unwrap();
     let at = a.t();
     let iter = at.whole_chunks((5, 4));
     for elt in iter {
         println!("{:6.2?}", elt);
     }
     let iter = at.whole_chunks((5, 4));
-    /*
-    */
     let mut b = <Array2<f32>>::zeros((2, 2));
     println!("{:?}", 
         Zip::from(&mut b).and(iter.clone())
     );
 
     azip!(mut b, ref a (iter) in { println!("{:6.2?}", a); *b = a.row(0).scalar_sum() });
+
     println!("{:?}", b);
     Zip::from(b.view_mut().reversed_axes()).and(a.whole_chunks([4, 5])).apply(|b, a| {
         println!("{:6.2?}", a);
         *b = a.row(0).scalar_sum();
     });
     println!("{:?}", b);
+
     //azip!(mut a (a), mut b in { *b = a.scalar_sum() });
 }
