81eb12d8735f207c5cb625a89f136c45fd044585
bluss <bluss@users.noreply.github.com>
2017-03-28 21:04:01 +0200

    Add benchmark for “clip”

---
+++ benches/numeric.rs
@@ -0,0 +1,27 @@
+
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+extern crate ndarray;
+use ndarray::prelude::*;
+
+const N: usize = 1024;
+const X: usize = 64;
+const Y: usize = 16;
+
+#[bench]
+fn clip(bench: &mut Bencher)
+{
+    let mut a = Array::linspace(0., 127., N * 2).into_shape([X, Y * 2]).unwrap();
+    let min = 2.;
+    let max = 5.;
+    bench.iter(|| {
+        a.mapv_inplace(|mut x| {
+            if x < min { x = min }
+            if x > max { x = max }
+            x
+        })
+    });
+}
