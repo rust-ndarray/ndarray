e365a70ea7d7980e9396193fac8085b33543239d
bluss <bluss@users.noreply.github.com>
2017-04-09 02:17:58 +0200

    Add benchmarks for gemv

---
+++ benches/gemv.rs
@@ -0,0 +1,42 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+extern crate ndarray;
+use ndarray::prelude::*;
+
+use ndarray::linalg::general_mat_vec_mul;
+
+#[bench]
+fn gemv_64_64c(bench: &mut Bencher) {
+    let a = Array::zeros((64, 64));
+    let (m, n) = a.dim();
+    let x = Array::zeros(n);
+    let mut y = Array::zeros(m);
+    bench.iter(|| {
+        general_mat_vec_mul(1.0, &a, &x, 1.0, &mut y);
+    });
+}
+
+#[bench]
+fn gemv_64_64f(bench: &mut Bencher) {
+    let a = Array::zeros((64, 64).f());
+    let (m, n) = a.dim();
+    let x = Array::zeros(n);
+    let mut y = Array::zeros(m);
+    bench.iter(|| {
+        general_mat_vec_mul(1.0, &a, &x, 1.0, &mut y);
+    });
+}
+
+#[bench]
+fn gemv_64_32(bench: &mut Bencher) {
+    let a = Array::zeros((64, 32));
+    let (m, n) = a.dim();
+    let x = Array::zeros(n);
+    let mut y = Array::zeros(m);
+    bench.iter(|| {
+        general_mat_vec_mul(1.0, &a, &x, 1.0, &mut y);
+    });
+}
