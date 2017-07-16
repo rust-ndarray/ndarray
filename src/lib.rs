ca1e4f50b8a0e4b027a9a709b32b06baf00aa599
bluss <bluss@users.noreply.github.com>
2017-07-17 01:10:02 +0200

    MAINT: Mark new versions for crates for ndarray 0.10

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 #![crate_name="ndarray"]
-#![doc(html_root_url = "http://bluss.github.io/rust-ndarray/master/")]
+#![doc(html_root_url = "https://docs.rs/ndarray/0.10/")]
 
 //! The `ndarray` crate provides an N-dimensional container for general elements
 //! and for numerics.
