9d19f1d5c8f2a808219f4930ab38cffd5b3119c1
bluss <bluss@users.noreply.github.com>
2017-08-11 20:49:43 +0200

    MAINT: Add another example (roll axis)

---
+++ examples/rollaxis.rs
@@ -0,0 +1,39 @@
+
+#[macro_use]
+extern crate ndarray;
+
+use ndarray::prelude::*;
+use ndarray::Data;
+
+pub fn roll_axis<A, S, D>(mut a: ArrayBase<S, D>, to: Axis, from: Axis) -> ArrayBase<S, D>
+    where S: Data<Elem=A>,
+          D: Dimension,
+{
+    let i = to.index();
+    let mut j = from.index();
+    if j > i {
+        while i != j {
+            a.swap_axes(i, j);
+            j -= 1;
+        }
+    } else {
+        while i != j {
+            a.swap_axes(i, j);
+            j += 1;
+        }
+    }
+    a
+}
+
+fn main() {
+    let mut data = array![[[-1., 0., -2.], [1., 7., -3.]],
+                          [[ 1., 0., -3.], [1., 7.,  5.]],
+                          [[ 1., 0., -3.], [1., 7.,  5.]],
+                          [[ 2., 0.,  2.], [1., 7.,  2.]]];
+
+    println!("{:8.4?}", data);
+
+    let data = roll_axis(data, Axis(2), Axis(0));
+
+    println!("{:8.4?}", data);
+}
