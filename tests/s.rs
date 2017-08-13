ec939afca72351154757323faf495fbdef48a28c
bluss <bluss@users.noreply.github.com>
2017-08-13 16:12:50 +0200

    MAINT: Add tests for trailing comma in s![]

---
+++ tests/s.rs
@@ -0,0 +1,17 @@
+
+#[macro_use(s)]
+extern crate ndarray;
+
+use ndarray::Array;
+
+#[test]
+fn test_s()
+{
+    let a = Array::<usize, _>::zeros((3, 4));
+    let vi = a.slice(s![1.., ..;2]);
+    assert_eq!(vi.shape(), &[2, 2]);
+
+    // trailing comma
+    let vi = a.slice(s![1.., ..;2, ]);
+    assert_eq!(vi.shape(), &[2, 2]);
+}
