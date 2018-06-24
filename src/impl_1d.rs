b1ce4cceda421703d0f33f7610ea3ed9d4acff23
LukeMathWalker <lucapalmieri1993@hotmail.com>
2018-06-24 19:17:39 +0100

    Merged tests. Changed test assertions

--- src/impl_1d.rs
+++ src/impl_1d.rs
@@ -113,27 +113,3 @@ impl<A, S> ArrayBase<S, Ix1>
         i-1
     }
 }
-
-#[test]
-fn test_partition_mut() {
-    let mut l = vec!(
-        arr1(&[1, 3, 2, 10, 10]),
-        arr1(&[355, 453, 452, 391, 289, 343,  44, 154, 271,  44, 314, 276, 160,
-               469, 191, 138, 163, 308, 395,   3, 416, 391, 210, 354, 200]),
-        arr1(&[ 84, 192, 216, 159,  89, 296,  35, 213, 456, 278,  98,  52, 308,
-               418, 329, 173, 286, 106, 366, 129, 125, 450,  23, 463, 151]),
-    );
-
-    for a in l.iter_mut() {
-        let n = a.len();
-        let pivot_index = n-1;
-        let pivot_value = a[pivot_index].clone();
-        let partition_index = a.partition_mut(pivot_index);
-        for i in 0..partition_index+1 {
-            assert!(a[i] <= pivot_value);
-        }
-        for j in (partition_index+1)..n {
-            assert!(pivot_value <= a[j]);
-        }
-    }
-}
--- tests/array.rs
+++ tests/array.rs
@@ -1705,20 +1705,27 @@ fn array_macros() {
 
 #[test]
 fn test_partition_mut() {
-    let mut a = arr1(&[1, 3, 2, 10, 10]);
-    let n = a.len();
-    let j = a.view_mut().partition_mut(n-1);
-    assert_eq!(j, 3);
-    for i in 0..j {
-        assert!(a[i] <= a[j]);
-    }
-    let mut a = arr1(&[2, 3, 4, 1]);
-    let n = a.len();
-    let j = a.view_mut().partition_mut(n-1);
-    assert_eq!(j, 0);
-    let n = a.len();
-    for i in j+1..n {
-        assert!(a[i] > a[j]);
+    let mut l = vec!(
+        arr1(&[1, 3, 2, 10, 10]),
+        arr1(&[2, 3, 4, 1]),
+        arr1(&[355, 453, 452, 391, 289, 343,  44, 154, 271,  44, 314, 276, 160,
+               469, 191, 138, 163, 308, 395,   3, 416, 391, 210, 354, 200]),
+        arr1(&[ 84, 192, 216, 159,  89, 296,  35, 213, 456, 278,  98,  52, 308,
+               418, 329, 173, 286, 106, 366, 129, 125, 450,  23, 463, 151]),
+    );
+
+    for a in l.iter_mut() {
+        let n = a.len();
+        let pivot_index = n-1;
+        let pivot_value = a[pivot_index].clone();
+        let partition_index = a.partition_mut(pivot_index);
+        for i in 0..partition_index {
+            assert!(a[i] < pivot_value);
+        }
+        assert!(a[partition_index] == pivot_value);
+        for j in (partition_index+1)..n {
+            assert!(pivot_value <= a[j]);
+        }
     }
 }
 
