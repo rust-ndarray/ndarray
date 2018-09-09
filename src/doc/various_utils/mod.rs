9b01eaf5d46c214d1a3916e87c51f059874ae329
torstein <t.soernes@gmail.com>
2018-09-09 21:59:46 +0200

    Explain sorting for non-contiguous view

--- src/doc/various_utils/mod.rs
+++ src/doc/various_utils/mod.rs
@@ -6,6 +6,7 @@
 //! As a basic example, here is how to sort an array of integers using `sort_unstable`
 //! from the standard library:
 //! ```
+//! extern crate ndarray;
 //! use ndarray::*;
 //! let mut arr: Array1<i64> = Array::from_vec(vec![3, 2, 5, 1]);
 //! arr.as_slice_mut().unwrap().sort_unstable();
@@ -14,6 +15,7 @@
 //! If we wish to sort floating point numbers, then we must specify how
 //! to handle sorting `NaN`s or `inf`s.
 //! ```
+//! use ndarray::*;
 //! let mut arr: Array1<f64> = Array::from_vec(vec![3.0, 2.0, 5.0, 1.0]);
 //! arr.as_slice_mut()
 //!     .unwrap()
@@ -26,6 +28,7 @@
 //! We can perform an argsort, that is, retrieving the indecies
 //! that would sort the array.
 //! ```
+//! use ndarray::*;
 //! fn argsort<D: Dimension, E: Ord>(arr: &Array<E, D>) -> Array1<usize> {
 //!     let mut zipped: Array1<(usize, &E)> = arr.into_iter().enumerate().collect();
 //!     zipped
@@ -39,6 +42,8 @@
 //! # Shuffling
 //! Similarly, shuffling can be performed using the [Rand Crate][https://crates.io/crates/rand]
 //! ```
+//! extern crate rand;
+//! use ndarray::*;
 //! use rand::{thread_rng, Rng};
 //! let mut arr: Array1<f64> = Array::from_vec(vec![3.0, 2.0, 5.0, 1.0]);
 //! thread_rng().shuffle(arr.as_slice_mut().unwrap());
@@ -46,6 +51,8 @@
 //!
 //! Two arrays can be shuffled in unison, at the cost of copying both arrays:
 //! ```
+//! use ndarray::*;
+//! use rand::{thread_rng, Rng};
 //! let arr1: Array1<f64> = Array::from_vec(vec![3.0, -1.0, 8.0, 2.0]);
 //! let arr2: Array1<f64> = Array::from_vec(vec![3.0, 2.0, 5.0, 1.0]);
 //!
@@ -54,3 +61,21 @@
 //! let arr1 = arr1.select(Axis(0), &indecies);
 //! let arr2 = arr2.select(Axis(0), &indecies);
 //! ```
+//!
+//! # On mutating non-contiguous vs. contiguous arrays
+//! Approaches using [`.as_slice_mut()`][https://docs.rs/ndarray/0.12.0/ndarray/struct.ArrayBase.html#method.as_slice_mut] only work if
+//! the array/view is contiguous. This means that, for example, trying to sort a column in a 2-D row-major array doesn't work with this approach.
+//!```
+//!use ndarray::prelude::*;
+//!
+//!fn main() {
+//!    let mut a = arr2(&[[1, 2], [3, 4]]);
+//!    a.column_mut(0).as_slice_mut().unwrap().sort_unstable();
+//!}
+//!```
+//!This panics with "thread 'main' panicked at 'called `Option::unwrap()` on a `None` value'" since the column is non-contiguous.
+//!
+//! There is not yet a good way to sort/shuffle non-contiguous arrays/views. If the array/view is non-contiguous, there are basically three options at the moment:
+//! * Copy the data into a continuous Vec/array/view, and then sort/shuffle the copy. This doesn't modify the order of the original data.
+//! * Sort/shuffle a slice of indices with .sort_by_key()/.shuffle(), and then rearrange the data according to the sorted/shuffled indices. examples/sort-axis.rs provides an example of this.
+//! * Add support for sorting strided data to a sorting crate or contribute the necessary methods to ndarray. :
