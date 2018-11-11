60d3b973930a83ee220953dabefc3996ec4a4b22
Jim Turner <github@turner.link>
2018-11-11 16:22:50 -0500

    Add into_scalar method for Array0

--- src/impl_owned_array.rs
+++ src/impl_owned_array.rs
@@ -1,5 +1,44 @@
 use imp_prelude::*;
 
+/// Methods specific to `Array0`.
+///
+/// ***See also all methods for [`ArrayBase`]***
+///
+/// [`ArrayBase`]: struct.ArrayBase.html
+impl<A> Array<A, Ix0> {
+    /// Returns the single element in the array without cloning it.
+    ///
+    /// ```
+    /// use ndarray::{arr0, Array0};
+    ///
+    /// // `Foo` doesn't implement `Clone`.
+    /// #[derive(Debug, Eq, PartialEq)]
+    /// struct Foo;
+    ///
+    /// let array: Array0<Foo> = arr0(Foo);
+    /// let scalar: Foo = array.into_scalar();
+    /// assert_eq!(scalar, Foo);
+    /// ```
+    pub fn into_scalar(mut self) -> A {
+        let size = ::std::mem::size_of::<A>();
+        if size == 0 {
+            // Any index in the `Vec` is fine since all elements are identical.
+            self.data.0.remove(0)
+        } else {
+            // Find the index in the `Vec` corresponding to `self.ptr`.
+            // (This is necessary because the element in the array might not be
+            // the first element in the `Vec`, such as if the array was created
+            // by `array![1, 2, 3, 4].slice_move(s![2])`.)
+            let first = self.ptr as usize;
+            let base = self.data.0.as_ptr() as usize;
+            let index = (first - base) / size;
+            debug_assert_eq!((first - base) % size, 0);
+            // Remove the element at the index and return it.
+            self.data.0.remove(index)
+        }
+    }
+}
+
 /// Methods specific to `Array`.
 ///
 /// ***See also all methods for [`ArrayBase`]***
