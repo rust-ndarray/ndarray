c0df25398443e954346d915c316c88ef11f73db5
ManifoldFR <wilson.jallet@gmail.com>
2019-01-14 11:20:40 +0100

    add dimension test for standard normal

--- ndarray-rand/src/multivariatenormal.rs
+++ ndarray-rand/src/multivariatenormal.rs
@@ -1,7 +1,7 @@
 //! Implementation of the multiavariate normal distribution.
 use super::RandomExt;
 use ndarray::Ix1;
-use ndarray::{Array1, Array2};
+use ndarray::Array1;
 use rand::Rng;
 use rand::distributions::{Distribution, StandardNormal};
 
@@ -19,8 +19,7 @@ impl MultivariateStandardNormal
     }
 }
 
-impl Distribution<Array1<f64>> for MultivariateStandardNormal
-{
+impl Distribution<Array1<f64>> for MultivariateStandardNormal {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Array1<f64> {
         let shape = self.shape.clone();
         let res = Array1::random_using(
@@ -31,7 +30,41 @@ impl Distribution<Array1<f64>> for MultivariateStandardNormal
 
 #[cfg(feature = "normal-dist")]
 /// The normal distribution `N(mean, covariance)`.
-pub struct MultivariateNormal {
-    mean: Array1<f64>,
-    covariance: Array2<f64>
+mod advanced_normal {
+    use rand::Rng;
+    use rand::distributions::{
+        Distribution, StandardNormal
+    };
+    use crate::RandomExt;
+    use ndarray::{Ix1, Array1, Array2};
+    use ndarray_linalg::cholesky::*;
+
+    pub struct MultivariateNormal {
+        shape: Ix1,
+        mean: Array1<f64>,
+        covariance: Array2<f64>
+    }
+
+    impl MultivariateNormal {
+        pub fn new(mean: Array1<f64>, covariance: Array2<f64>) -> Self {
+            let shape = mean.shape() as Ix1;
+            assert_eq!(shape[0], covariance.shape()[0]);
+            MultivariateNormal {
+                shape, mean, covariance
+            }
+        }
+    }
+
+    impl Distribution<Array1<f64>> for MultivariateNormal {
+        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Array1<f64> {
+            let shape = self.shape.clone();
+            // standard normal distribution
+            let res = Array1::random_using(
+                shape, StandardNormal, rng);
+            // use Cholesky decomposition to obtain a sample of our general multivariate normal
+            let l: Array2<f64> = self.covariance.view().cholesky(UPLO::Lower).unwrap();
+            self.mean.view() + l * res
+        }
+    }
+
 }
--- ndarray-rand/tests/tests.rs
+++ ndarray-rand/tests/tests.rs
@@ -3,9 +3,12 @@ extern crate rand;
 extern crate ndarray;
 extern crate ndarray_rand;
 
-use rand::distributions::Uniform;
+use rand::distributions::{
+    Uniform, Distribution
+};
 use ndarray::Array;
 use ndarray_rand::RandomExt;
+use ndarray_rand::multivariatenormal::MultivariateStandardNormal;
 
 #[test]
 fn test_dim() {
@@ -18,4 +21,14 @@ fn test_dim() {
             assert!(a.iter().all(|x| *x >= 0.));
         }
     }
+}
+
+#[test]
+fn test_standard_normal() {
+    use ndarray::IntoDimension;
+    let shape = (2usize,).into_dimension();
+    let n = MultivariateStandardNormal::new(shape.into());
+    let ref mut rng = rand::thread_rng();
+    let s: ndarray::Array1<f64> = n.sample(rng);
+    assert_eq!(s.shape(), &[2]);
 }
