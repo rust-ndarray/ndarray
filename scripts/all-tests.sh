e7b00697683309c91f5a5d2eae27c59f7eff6239
Maximilian Roos <m@maxroos.com>
2019-06-14 00:59:20 -0400

    only run clippy on nightly

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -14,4 +14,5 @@ cargo test --verbose --features "$FEATURES"
 cargo test --manifest-path=serialization-tests/Cargo.toml --verbose
 cargo test --manifest-path=blas-tests/Cargo.toml --verbose
 CARGO_TARGET_DIR=target/ cargo test --manifest-path=numeric-tests/Cargo.toml --verbose
+([ "$IS_NIGHTLY" != 1 ] || cargo clippy)
 ([ "$IS_NIGHTLY" != 1 ] || cargo bench --no-run --verbose --features "$FEATURES")
