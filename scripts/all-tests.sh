e2d86aad783779ffa8d3b875d42029115ec65875
Maximilian Roos <m@maxroos.com>
2019-06-21 14:50:14 -0400

    clippy on beta only since sometimes not available on nightly (e.g. today)

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 FEATURES=$1
-IS_NIGHTLY=$2
+CHANNEL=$2
 
 cargo build --verbose --no-default-features
 cargo test --verbose --no-default-features
@@ -14,5 +14,5 @@ cargo test --verbose --features "$FEATURES"
 cargo test --manifest-path=serialization-tests/Cargo.toml --verbose
 cargo test --manifest-path=blas-tests/Cargo.toml --verbose
 CARGO_TARGET_DIR=target/ cargo test --manifest-path=numeric-tests/Cargo.toml --verbose
-([ "$IS_NIGHTLY" != 1 ] || cargo clippy)
-([ "$IS_NIGHTLY" != 1 ] || cargo bench --no-run --verbose --features "$FEATURES")
+([ "$CHANNEL" != "beta" ] || cargo clippy)
+([ "$CHANNEL" != "nightly" ] || cargo bench --no-run --verbose --features "$FEATURES")
