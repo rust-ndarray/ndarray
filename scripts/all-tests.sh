9bce28d0f2d8adafb73421331026f8892056c4e5
Maximilian Roos <m@maxroos.com>
2019-06-22 13:58:02 -0400

    bash precedence

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -14,5 +14,5 @@ cargo test --verbose --features "$FEATURES"
 cargo test --manifest-path=serialization-tests/Cargo.toml --verbose
 cargo test --manifest-path=blas-tests/Cargo.toml --verbose
 CARGO_TARGET_DIR=target/ cargo test --manifest-path=numeric-tests/Cargo.toml --verbose
-([ "$CHANNEL" != "beta" ] || rustup component add clippy && cargo clippy)
+([ "$CHANNEL" != "beta" ] || (rustup component add clippy && cargo clippy))
 ([ "$CHANNEL" != "nightly" ] || cargo bench --no-run --verbose --features "$FEATURES")
