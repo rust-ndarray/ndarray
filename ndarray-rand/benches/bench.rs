1462c5e1bd67137e6baacdb5cf5a76f1f5ebb1aa
Maximilian Roos <m@maxroos.com>
2019-07-18 14:26:13 -0400

    cargo fmt

--- ndarray-rand/benches/bench.rs
+++ ndarray-rand/benches/bench.rs
@@ -9,31 +9,25 @@ extern crate test;
 use ndarray::Array;
 use ndarray_rand::RandomExt;
 use ndarray_rand::F32;
-use rand_distr::Normal;
 use rand::distributions::Uniform;
+use rand_distr::Normal;
 
 use test::Bencher;
 
 #[bench]
 fn uniform_f32(b: &mut Bencher) {
     let m = 100;
-    b.iter(|| {
-        Array::random((m, m), Uniform::new(-1f32, 1.))
-    });
+    b.iter(|| Array::random((m, m), Uniform::new(-1f32, 1.)));
 }
 
 #[bench]
 fn norm_f32(b: &mut Bencher) {
     let m = 100;
-    b.iter(|| {
-        Array::random((m, m), F32(Normal::new(0., 1.)))
-    });
+    b.iter(|| Array::random((m, m), F32(Normal::new(0., 1.))));
 }
 
 #[bench]
 fn norm_f64(b: &mut Bencher) {
     let m = 100;
-    b.iter(|| {
-        Array::random((m, m), Normal::new(0., 1.))
-    });
+    b.iter(|| Array::random((m, m), Normal::new(0., 1.)));
 }
