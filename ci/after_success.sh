deddcd9bb0e662636d8f7f2a159acbcfad3ab447
JP-Ellis <josh@jpellis.me>
2019-09-03 16:53:07 +1000

    Remove coveralls support in favour of only codecov
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -7,28 +7,22 @@ set -o nounset
 # Exit on any error
 set -o errexit
 
-# We only need to run the coverage suite once
-COVERAGE_RUN=false
 KCOV_BIN="./target/kcov-master/build/src/kcov"
 
 run_kcov() {
     # Run kcov on all the test suites
-    if [[ $COVERAGE_RUN != "true" ]]; then
-        cargo coverage --all --tests --benches --no-default-features
-        mv target/kcov target/kcov-no-default-features
-        cargo coverage --all --tests --benches --features "$FEATURES"
-        mv target/kcov target/kcov-features
+    cargo coverage --all --tests --benches --no-default-features
+    mv target/kcov target/kcov-no-default-features
+    cargo coverage --all --tests --benches --features "$FEATURES"
+    mv target/kcov target/kcov-features
 
-        $KCOV_BIN --coveralls-id $TRAVIS_JOB_ID --merge target/kcov \
-                  target/kcov-no-default-features \
-                  target/kcov-features
+    $KCOV_BIN --merge target/kcov \
+              target/kcov-no-default-features \
+              target/kcov-features
 
-        rm -rf \
-           target/kcov-no-default-features \
-           target/kcov-features
-
-        COVERAGE_RUN=true
-    fi
+    rm -rf \
+       target/kcov-no-default-features \
+       target/kcov-features
 }
 
 coverage_codecov() {
@@ -42,18 +36,7 @@ coverage_codecov() {
     echo "Uploaded code coverage to codecov.io"
 }
 
-coverage_coveralls() {
-    if [[ "$TRAVIS_RUST_VERSION" != "stable" ]]; then
-        return
-    fi
-
-    run_kcov
-
-    # Data is automatically uploaded by kcov
-}
-
 main() {
-    coverage_coveralls
     coverage_codecov
 }
 
