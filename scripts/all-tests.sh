d09710b2ef4e4b7cba0a94acdcd715dc39ad4d50
Maximilian Roos <m@maxroos.com>
2019-09-12 09:08:09 -0400

    test parallel

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -7,7 +7,6 @@ FEATURES=$1
 CHANNEL=$2
 
 ([ "$CHANNEL" != "beta" ] || (rustup component add rustfmt && cargo fmt --all -- --check))
-export RUSTFLAGS="-D warnings"
 cargo build --verbose --no-default-features
 # Testing both dev and release profiles helps find bugs, especially in low level code
 cargo test --verbose --no-default-features
@@ -16,6 +15,7 @@ cargo build --verbose --features "$FEATURES"
 cargo test --verbose --features "$FEATURES"
 cargo test --manifest-path=serialization-tests/Cargo.toml --verbose
 cargo test --manifest-path=blas-tests/Cargo.toml --verbose
+cargo test --manifest-path=parallel/Cargo.toml --verbose
 CARGO_TARGET_DIR=target/ cargo test --manifest-path=numeric-tests/Cargo.toml --verbose
 ([ "$CHANNEL" != "beta" ] || (rustup component add clippy && cargo clippy))
 ([ "$CHANNEL" != "nightly" ] || cargo bench --no-run --verbose --features "$FEATURES")
