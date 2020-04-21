a659235cd5d325500e3227570664f13e0d6da14e
bluss <bluss@users.noreply.github.com>
2020-04-21 22:03:08 +0200

    FEAT: Keep track of "layout tendency" in Zip for better performance
    Support both unroll over c- and f-layout preferred axis in Zip inner loop
    (the fallback when inputs are not all contiguous and same layout).
    
    Keep a tendency score when building the Zip, so that we know if the
    inputs are tending to be c- or f- layout.
    
    This improves performance on the just added zip_indexed_ff benchmark, so
    that it seems to match its (already fast) cc counterpart.

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1,6 +1,8 @@
 mod layoutfmt;
 
-// public struct but users don't interact with it
+// Layout it a bitset used for internal layout description of
+// arrays, producers and sets of producers.
+// The type is public but users don't interact with it.
 #[doc(hidden)]
 /// Memory layout description
 #[derive(Copy, Clone)]
@@ -8,44 +10,62 @@ pub struct Layout(u32);
 
 impl Layout {
     #[inline(always)]
-    pub(crate) fn new(x: u32) -> Self {
-        Layout(x)
-    }
-
-    #[inline(always)]
     pub(crate) fn is(self, flag: u32) -> bool {
         self.0 & flag != 0
     }
+
+    /// Return layout common to both inputs
     #[inline(always)]
-    pub(crate) fn and(self, flag: Layout) -> Layout {
-        Layout(self.0 & flag.0)
+    pub(crate) fn intersect(self, other: Layout) -> Layout {
+        Layout(self.0 & other.0)
     }
 
+    /// Return a layout that simultaneously "is" what both of the inputs are
     #[inline(always)]
-    pub(crate) fn flag(self) -> u32 {
-        self.0
+    pub(crate) fn also(self, other: Layout) -> Layout {
+        Layout(self.0 | other.0)
     }
 
     #[inline(always)]
     pub(crate) fn one_dimensional() -> Layout {
-        Layout(CORDER | FORDER)
+        Layout::c().also(Layout::f())
     }
 
     #[inline(always)]
     pub(crate) fn c() -> Layout {
-        Layout(CORDER)
+        Layout(CORDER | CPREFER)
     }
 
     #[inline(always)]
     pub(crate) fn f() -> Layout {
-        Layout(FORDER)
+        Layout(FORDER | FPREFER)
+    }
+
+    #[inline(always)]
+    pub(crate) fn cpref() -> Layout {
+        Layout(CPREFER)
+    }
+
+    #[inline(always)]
+    pub(crate) fn fpref() -> Layout {
+        Layout(FPREFER)
     }
 
     #[inline(always)]
     pub(crate) fn none() -> Layout {
         Layout(0)
     }
+
+    /// A simple "score" method which scores positive for preferring C-order, negative for F-order
+    /// Subject to change when we can describe other layouts
+    pub(crate) fn tendency(self) -> i32 {
+        (self.is(CORDER) as i32 - self.is(FORDER) as i32) +
+        (self.is(CPREFER) as i32 - self.is(FPREFER) as i32)
+
+    }
 }
 
 pub const CORDER: u32 = 0b01;
 pub const FORDER: u32 = 0b10;
+pub const CPREFER: u32 = 0b0100;
+pub const FPREFER: u32 = 0b1000;
