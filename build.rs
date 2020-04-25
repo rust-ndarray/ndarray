48292bc4b796d948b7571dd4d5741b7db21e3e27
bluss <bluss@users.noreply.github.com>
2020-04-25 12:38:12 +0200

    FIX: Clippy lints
    The clippy.toml setting is a workaround for the currently broken single
    char variable lint (a fix is pending in clippy).

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     if cfg!(feature = "test-blas-openblas-sys") {
-        println!("cargo:rustc-link-lib={}=openblas", "dylib");
+        println!("cargo:rustc-link-lib=dylib=openblas");
     }
 }
