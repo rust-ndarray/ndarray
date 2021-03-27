86accd6264c572a361794837552e18937846caad
bluss <bluss@users.noreply.github.com>
2021-03-27 19:08:36 +0100

    MAINT: Update blas test for blas-src 0.8
    The version decoupling in ndarray 0.15 was a success, now it's easy to
    use ndarray with blas-src 0.7 or 0.8, for example. Update docs and
    xblas-test (blas-tests) with the new versions.

---
+++ xtest-blas/src/lib.rs
@@ -0,0 +1,6 @@
+
+#[cfg(not(feature = "blas-src"))]
+compile_error!("Missing backend: could not compile.
+       Help: For this testing crate, select one of the blas backend features, for example \
+             openblas-system");
+
