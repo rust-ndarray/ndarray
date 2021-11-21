0fe046df5dce194edfd5349181d603ad8d39551e
Ulrik Sverdrup <bluss@users.noreply.github.com>
2021-11-21 12:52:23 +0100

    Use matrixmultiply cgemm and improve tests
    Refactor tests to use generics in a bit smarter way.
    
    We test both .dot() and general_mat_mul. Tests now use relative accuracy
    more explicitly (this works better with generics, instead of using
    approx).

--- xtest-numeric/tests/accuracy.rs
+++ xtest-numeric/tests/accuracy.rs
@@ -6,6 +6,8 @@ extern crate rand;
 
 extern crate numeric_tests;
 
+use std::fmt;
+
 use ndarray_rand::RandomExt;
 use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
@@ -17,9 +19,27 @@ use ndarray::{
 };
 use ndarray::linalg::general_mat_mul;
 
-use rand_distr::Normal;
+use rand_distr::{Normal, StandardNormal, Distribution};
+use num_traits::{Float, AsPrimitive};
+use num_complex::Complex;
 
 use approx::{assert_abs_diff_eq, assert_relative_eq};
+
+fn kahan_sum<A>(iter: impl Iterator<Item = A>) -> A
+    where A: LinalgScalar
+{
+    let mut sum = A::zero();
+    let mut compensation = A::zero();
+
+    for elt in iter {
+        let y = elt - compensation;
+        let t = sum + y;
+        compensation = (t - sum) - y;
+        sum = t;
+    }
+
+    sum
+}
 
 // simple, slow, correct (hopefully) mat mul
 fn reference_mat_mul<A, S, S2>(lhs: &ArrayBase<S, Ix2>, rhs: &ArrayBase<S2, Ix2>)
@@ -29,46 +49,48 @@ fn reference_mat_mul<A, S, S2>(lhs: &ArrayBase<S, Ix2>, rhs: &ArrayBase<S2, Ix2>
           S2: Data<Elem=A>,
 {
     let ((m, k), (_, n)) = (lhs.dim(), rhs.dim());
-    let mut res_elems = Vec::<A>::with_capacity(m * n);
-    unsafe {
-        res_elems.set_len(m * n);
-    }
+    let mut res_elems = Array::zeros(m * n);
 
     let mut i = 0;
     let mut j = 0;
     for rr in &mut res_elems {
-        unsafe {
-            *rr = (0..k).fold(A::zero(),
-                move |s, x| s + *lhs.uget((i, x)) * *rhs.uget((x, j)));
-        }
+        let lhs_i = lhs.row(i);
+        let rhs_j = rhs.column(j);
+        *rr = kahan_sum((0..k).map(move |x| lhs_i[x] * rhs_j[x]));
+
         j += 1;
         if j == n {
             j = 0;
             i += 1;
         }
     }
-    unsafe {
-        ArrayBase::from_shape_vec_unchecked((m, n), res_elems)
-    }
+
+    res_elems.into_shape((m, n)).unwrap()
 }
 
-fn gen<D>(d: D) -> Array<f32, D>
+fn gen<A, D>(d: D, rng: &mut SmallRng) -> Array<A, D>
     where D: Dimension,
+          A: Float,
+          StandardNormal: Distribution<A>,
 {
-    Array::random(d, Normal::new(0., 1.).unwrap())
+    Array::random_using(d, Normal::new(A::zero(), A::one()).unwrap(), rng)
 }
-fn gen_f64<D>(d: D) -> Array<f64, D>
+
+fn gen_complex<A, D>(d: D, rng: &mut SmallRng) -> Array<Complex<A>, D>
     where D: Dimension,
+          A: Float,
+          StandardNormal: Distribution<A>,
 {
-    Array::random(d, Normal::new(0., 1.).unwrap())
+    gen(d.clone(), rng).mapv(Complex::from) + gen(d, rng).mapv(|x| Complex::new(A::zero(), x))
 }
 
 #[test]
 fn accurate_eye_f32() {
+    let rng = &mut SmallRng::from_entropy();
     for i in 0..20 {
         let eye = Array::eye(i);
         for j in 0..20 {
-            let a = gen(Ix2(i, j));
+            let a = gen::<f32, _>(Ix2(i, j), rng);
             let a2 = eye.dot(&a);
             assert_abs_diff_eq!(a, a2, epsilon = 1e-6);
             let a3 = a.t().dot(&eye);
@@ -76,12 +98,11 @@ fn accurate_eye_f32() {
         }
     }
     // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
     for _ in 0..10 {
         let i = rng.gen_range(15..512);
         let j = rng.gen_range(15..512);
         println!("Testing size {} by {}", i, j);
-        let a = gen(Ix2(i, j));
+        let a = gen::<f32, _>(Ix2(i, j), rng);
         let eye = Array::eye(i);
         let a2 = eye.dot(&a);
         assert_abs_diff_eq!(a, a2, epsilon = 1e-6);
@@ -92,11 +113,12 @@ fn accurate_eye_f32() {
 
 #[test]
 fn accurate_eye_f64() {
+    let rng = &mut SmallRng::from_entropy();
     let abs_tol = 1e-15;
     for i in 0..20 {
         let eye = Array::eye(i);
         for j in 0..20 {
-            let a = gen_f64(Ix2(i, j));
+            let a = gen::<f64, _>(Ix2(i, j), rng);
             let a2 = eye.dot(&a);
             assert_abs_diff_eq!(a, a2, epsilon = abs_tol);
             let a3 = a.t().dot(&eye);
@@ -104,12 +126,11 @@ fn accurate_eye_f64() {
         }
     }
     // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
     for _ in 0..10 {
         let i = rng.gen_range(15..512);
         let j = rng.gen_range(15..512);
         println!("Testing size {} by {}", i, j);
-        let a = gen_f64(Ix2(i, j));
+        let a = gen::<f64, _>(Ix2(i, j), rng);
         let eye = Array::eye(i);
         let a2 = eye.dot(&a);
         assert_abs_diff_eq!(a, a2, epsilon = 1e-6);
@@ -119,114 +140,125 @@ fn accurate_eye_f64() {
 }
 
 #[test]
-fn accurate_mul_f32() {
-    // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
-    for i in 0..20 {
-        let m = rng.gen_range(15..512);
-        let k = rng.gen_range(15..512);
-        let n = rng.gen_range(15..1560);
-        let a = gen(Ix2(m, k));
-        let b = gen(Ix2(n, k));
-        let b = b.t();
-        let (a, b) = if i > 10 {
-            (a.slice(s![..;2, ..;2]),
-             b.slice(s![..;2, ..;2]))
-        } else { (a.view(), b) };
-
-        println!("Testing size {} by {} by {}", a.shape()[0], a.shape()[1], b.shape()[1]);
-        let c = a.dot(&b);
-        let reference = reference_mat_mul(&a, &b);
-
-        assert_relative_eq!(c, reference, epsilon = 1e-4, max_relative = 1e-3);
-    }
+fn accurate_mul_f32_dot() {
+    accurate_mul_float_general::<f32>(1e-5, false);
 }
 
 #[test]
 fn accurate_mul_f32_general() {
-    // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
-    for i in 0..20 {
-        let m = rng.gen_range(15..512);
-        let k = rng.gen_range(15..512);
-        let n = rng.gen_range(15..1560);
-        let a = gen(Ix2(m, k));
-        let b = gen(Ix2(n, k));
-        let mut c = gen(Ix2(m, n));
-        let b = b.t();
-        let (a, b, mut c) = if i > 10 {
-            (a.slice(s![..;2, ..;2]),
-             b.slice(s![..;2, ..;2]),
-             c.slice_mut(s![..;2, ..;2]))
-        } else { (a.view(), b, c.view_mut()) };
-
-        println!("Testing size {} by {} by {}", a.shape()[0], a.shape()[1], b.shape()[1]);
-        general_mat_mul(1., &a, &b, 0., &mut c);
-        let reference = reference_mat_mul(&a, &b);
-
-        assert_relative_eq!(c, reference, epsilon = 1e-4, max_relative = 1e-3);
-    }
+    accurate_mul_float_general::<f32>(1e-5, true);
 }
 
 #[test]
-fn accurate_mul_f64() {
-    // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
-    for i in 0..20 {
-        let m = rng.gen_range(15..512);
-        let k = rng.gen_range(15..512);
-        let n = rng.gen_range(15..1560);
-        let a = gen_f64(Ix2(m, k));
-        let b = gen_f64(Ix2(n, k));
-        let b = b.t();
-        let (a, b) = if i > 10 {
-            (a.slice(s![..;2, ..;2]),
-             b.slice(s![..;2, ..;2]))
-        } else { (a.view(), b) };
-
-        println!("Testing size {} by {} by {}", a.shape()[0], a.shape()[1], b.shape()[1]);
-        let c = a.dot(&b);
-        let reference = reference_mat_mul(&a, &b);
-
-        assert_relative_eq!(c, reference, epsilon = 1e-12, max_relative = 1e-7);
-    }
+fn accurate_mul_f64_dot() {
+    accurate_mul_float_general::<f64>(1e-14, false);
 }
 
 #[test]
 fn accurate_mul_f64_general() {
+    accurate_mul_float_general::<f64>(1e-14, true);
+}
+
+/// Generate random sized matrices using the given generator function.
+/// Compute gemm using either .dot() (if use_general is false) otherwise general_mat_mul.
+/// Return tuple of actual result matrix and reference matrix, which should be equal.
+fn random_matrix_mul<A>(rng: &mut SmallRng, use_stride: bool, use_general: bool,
+                        generator: fn(Ix2, &mut SmallRng) -> Array2<A>)
+    -> (Array2<A>, Array2<A>)
+    where A: LinalgScalar,
+{
+    let m = rng.gen_range(15..512);
+    let k = rng.gen_range(15..512);
+    let n = rng.gen_range(15..1560);
+    let a = generator(Ix2(m, k), rng);
+    let b = generator(Ix2(n, k), rng);
+    let c = if use_general {
+        Some(generator(Ix2(m, n), rng))
+    } else {
+        None
+    };
+
+    let b = b.t();
+    let (a, b, mut c) = if use_stride {
+        (a.slice(s![..;2, ..;2]),
+         b.slice(s![..;2, ..;2]),
+         c.map(|c_| c_.slice_move(s![..;2, ..;2])))
+    } else {
+        (a.view(),
+         b,
+         c)
+    };
+
+    println!("Testing size {} by {} by {}", a.shape()[0], a.shape()[1], b.shape()[1]);
+    if let Some(c) = &mut c {
+        general_mat_mul(A::one(), &a, &b, A::zero(), c);
+    } else {
+        c = Some(a.dot(&b));
+    }
+    let c = c.unwrap();
+    let reference = reference_mat_mul(&a, &b);
+
+    (c, reference)
+}
+
+fn accurate_mul_float_general<A>(limit: f64, use_general: bool)
+    where A: Float + Copy + 'static + AsPrimitive<f64>,
+          StandardNormal: Distribution<A>,
+          A: fmt::Debug,
+{
     // pick a few random sizes
     let mut rng = SmallRng::from_entropy();
     for i in 0..20 {
-        let m = rng.gen_range(15..512);
-        let k = rng.gen_range(15..512);
-        let n = rng.gen_range(15..1560);
-        let a = gen_f64(Ix2(m, k));
-        let b = gen_f64(Ix2(n, k));
-        let mut c = gen_f64(Ix2(m, n));
-        let b = b.t();
-        let (a, b, mut c) = if i > 10 {
-            (a.slice(s![..;2, ..;2]),
-             b.slice(s![..;2, ..;2]),
-             c.slice_mut(s![..;2, ..;2]))
-        } else { (a.view(), b, c.view_mut()) };
+        let (c, reference) = random_matrix_mul(&mut rng, i > 10, use_general, gen::<A, _>);
 
-        println!("Testing size {} by {} by {}", a.shape()[0], a.shape()[1], b.shape()[1]);
-        general_mat_mul(1., &a, &b, 0., &mut c);
-        let reference = reference_mat_mul(&a, &b);
+        let diff = &c - &reference;
+        let max_diff = diff.iter().copied().fold(A::zero(), A::max);
+        let max_elt = reference.iter().copied().fold(A::zero(), A::max);
+        println!("Max elt diff={:?}, max={:?}, ratio={:.4e}", max_diff, max_elt, (max_diff/max_elt).as_());
+        assert!((max_diff / max_elt).as_() < limit,
+                "Expected relative norm diff < {:e}, found {:?} / {:?}", limit, max_diff, max_elt);
+    }
+}
 
-        assert_relative_eq!(c, reference, epsilon = 1e-12, max_relative = 1e-7);
+#[test]
+fn accurate_mul_complex32() {
+    accurate_mul_complex_general::<f32>(1e-5);
+}
+
+#[test]
+fn accurate_mul_complex64() {
+    accurate_mul_complex_general::<f64>(1e-14);
+}
+
+fn accurate_mul_complex_general<A>(limit: f64)
+    where A: Float + Copy + 'static + AsPrimitive<f64>,
+          StandardNormal: Distribution<A>,
+          A: fmt::Debug,
+{
+    // pick a few random sizes
+    let mut rng = SmallRng::from_entropy();
+    for i in 0..20 {
+        let (c, reference) = random_matrix_mul(&mut rng, i > 10, true, gen_complex::<A, _>);
+
+        let diff = &c - &reference;
+        let max_elt = |elt: &Complex<_>| A::max(A::abs(elt.re), A::abs(elt.im));
+        let max_diff = diff.iter().map(max_elt).fold(A::zero(), A::max);
+        let max_elt = reference.iter().map(max_elt).fold(A::zero(), A::max);
+        println!("Max elt diff={:?}, max={:?}, ratio={:.4e}", max_diff, max_elt, (max_diff/max_elt).as_());
+        assert!((max_diff / max_elt).as_() < limit,
+                "Expected relative norm diff < {:e}, found {:?} / {:?}", limit, max_diff, max_elt);
     }
 }
 
 #[test]
 fn accurate_mul_with_column_f64() {
     // pick a few random sizes
-    let mut rng = SmallRng::from_entropy();
+    let rng = &mut SmallRng::from_entropy();
     for i in 0..10 {
         let m = rng.gen_range(1..350);
         let k = rng.gen_range(1..350);
-        let a = gen_f64(Ix2(m, k));
-        let b_owner = gen_f64(Ix2(k, k));
+        let a = gen::<f64, _>(Ix2(m, k), rng);
+        let b_owner = gen::<f64, _>(Ix2(k, k), rng);
         let b_row_col;
         let b_sq;
 
