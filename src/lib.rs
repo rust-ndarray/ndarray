a7c3d9ee488dc7db67fc1690d3565cb20db81e20
Ulrik Sverdrup <bluss@users.noreply.github.com>
2021-11-23 20:56:05 +0100

    doc: Give crate feature flags its own doc page
    Tighten up the crate feature list on the main page, and link over to a
    page with more full information - where we can explain more. This gives
    more room to the main page.

--- src/lib.rs
+++ src/lib.rs
@@ -75,30 +75,15 @@
 //! ## Crate Feature Flags
 //!
 //! The following crate feature flags are available. They are configured in your
-//! `Cargo.toml`.
+//! `Cargo.toml`. See [`doc::crate_feature_flags`] for more information.
 //!
-//! - `std`
-//!   - Rust standard library (enabled by default)
-//!   - This crate can be used without the standard library by disabling the
-//!     default `std` feature. To do so, use `default-features = false` in
-//!     your `Cargo.toml`.
-//!   - The `geomspace` `linspace` `logspace` `range` `std` `var` `var_axis`
-//!     and `std_axis` methods are only available when `std` is enabled.
-//! - `serde`
-//!   - Enables serialization support for serde 1.x
-//! - `rayon`
-//!   - Enables parallel iterators, parallelized methods, the [`parallel`] module and [`par_azip!`].
-//!   - Implies std
-//! - `approx`
-//!   - Enables implementations of traits from version 0.4 of the [`approx`] crate.
-//! - `approx-0_5`
-//!   - Enables implementations of traits from version 0.5 of the [`approx`] crate.
-//! - `blas`
-//!   - Enable transparent BLAS support for matrix multiplication.
-//!     Uses ``blas-src`` for pluggable backend, which needs to be configured
-//!     separately (see the README).
-//! - `matrixmultiply-threading`
-//!   - Enable the ``threading`` feature in the matrixmultiply package
+//! - `std`: Rust standard library-using functionality (enabled by default)
+//! - `serde`: serialization support for serde 1.x
+//! - `rayon`: Parallel iterators, parallelized methods, the [`parallel`] module and [`par_azip!`].
+//! - `approx` Implementations of traits from version 0.4 of the [`approx`] crate.
+//! - `approx-0_5`: Implementations of traits from version 0.5 of the [`approx`] crate.
+//! - `blas`: transparent BLAS support for matrix multiplication, needs configuration.
+//! - `matrixmultiply-threading`: Use threading from `matrixmultiply`.
 //!
 //! ## Documentation
 //!
