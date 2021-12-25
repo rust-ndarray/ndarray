f444a2e05ec0555c61542351b3a62611565a4b81
bokutou <mushin.hudoushin@gmail.com>
2021-12-25 21:06:16 +0900

    add test negative strides

--- tests/shrink_to_fit.rs
+++ tests/shrink_to_fit.rs
@@ -1,7 +1,7 @@
 use ndarray::{s, Array};
 
 #[test]
-fn array3d_0_dim() {
+fn dim_0() {
     let mut raw_vec = Vec::new();
     for i in 0..4 * 5 * 6 {
         raw_vec.push(i);
@@ -14,7 +14,7 @@ fn array3d_0_dim() {
 }
 
 #[test]
-fn array3d_swap_axis_0_dim() {
+fn swap_axis_dim_0() {
     let mut raw_vec = Vec::new();
     for i in 0..4 * 5 * 6 {
         raw_vec.push(i);
@@ -28,7 +28,7 @@ fn array3d_swap_axis_0_dim() {
 }
 
 #[test]
-fn array3d_swap_axis_dim() {
+fn swap_axis_dim() {
     let mut raw_vec = Vec::new();
     for i in 0..4 * 5 * 6 {
         raw_vec.push(i);
@@ -36,6 +36,19 @@ fn array3d_swap_axis_dim() {
     let mut a = Array::from_shape_vec((4, 5, 6), raw_vec).unwrap();
     a.swap_axes(2, 1);
     let mut a_slice = a.slice_move(s![2..3, 0..3, 0..;2]);
+    let a_slice_clone = a_slice.view().to_owned();
+    a_slice.shrink_to_fit();
+    assert_eq!(a_slice, a_slice_clone);
+}
+
+#[test]
+fn stride_negative() {
+    let mut raw_vec = Vec::new();
+    for i in 0..4 * 5 * 6 {
+        raw_vec.push(i);
+    }
+    let mut a = Array::from_shape_vec((4, 5, 6), raw_vec).unwrap();
+    let mut a_slice = a.slice_move(s![2..3, 0..3, 0..;-1]);
     let a_slice_clone = a_slice.view().to_owned();
     a_slice.shrink_to_fit();
     assert_eq!(a_slice, a_slice_clone);
