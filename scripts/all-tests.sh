8fe9a48b072c2f4ccf0f9445774fc139a2f9fee3
Adam Reichold <adam.reichold@t-online.de>
2023-12-02 08:06:42 +0100

    Update forcing dependency versions to fix MSRV build.

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -7,17 +7,18 @@ FEATURES=$1
 CHANNEL=$2
 
 if [ "$CHANNEL" = "1.51.0" ]; then
+    cargo update --package openblas-src --precise 0.10.5
+    cargo update --package openblas-build --precise 0.10.5
+    cargo update --package once_cell --precise 1.14.0
+    cargo update --package byteorder --precise 1.4.3
     cargo update --package rayon --precise 1.5.3
     cargo update --package rayon-core --precise 1.9.3
-    cargo update --package quote --precise 1.0.30
-    cargo update --package proc-macro2 --precise 1.0.65
     cargo update --package rmp --precise 0.8.11
     cargo update --package serde_json --precise 1.0.99
     cargo update --package serde --precise 1.0.156
     cargo update --package thiserror --precise 1.0.39
-    cargo update --package once_cell --precise 1.14.0
-    cargo update --package openblas-src --precise 0.10.5
-    cargo update --package openblas-build --precise 0.10.5
+    cargo update --package quote --precise 1.0.30
+    cargo update --package proc-macro2 --precise 1.0.65
 fi
 
 cargo build --verbose --no-default-features
