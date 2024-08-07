2ca801c1c3c66159acafb41fad9c7ad6fb375402
Ulrik Sverdrup <bluss@users.noreply.github.com>
2024-08-07 11:18:44 +0200

    ndarray-gen: Add simple internal interface for building matrices

---
+++ crates/ndarray-gen/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2024 bluss and ndarray developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+/// Build ndarray arrays for test purposes
+
+pub mod array_builder;
