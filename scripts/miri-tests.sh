1d27f5acea15ed4e67e7d4fee3ff736e8640576d
Adam Kern <akern40@gmail.com>
2024-10-26 12:25:39 -0400

    Limits miri to just the basics, for now

--- scripts/miri-tests.sh
+++ scripts/miri-tests.sh
@@ -9,17 +9,12 @@ RUSTFLAGS="-Zrandomize-layout"
 
 # Miri reports a stacked borrow violation deep within rayon, in a crate called crossbeam-epoch
 # The crate has a PR to fix this: https://github.com/crossbeam-rs/crossbeam/pull/871
-# but using Miri's tree borrow mode may resolve it for now
-MIRIFLAGS="-Zmiri-tree-borrows"
+# but using Miri's tree borrow mode may resolve it for now.
+# Disabled until we can figure out a different rayon issue.
+# MIRIFLAGS="-Zmiri-tree-borrows"
 
 # General tests
-cargo miri test -v --features "$FEATURES"
-
-# BLAS tests
-cargo miri test -p ndarray --lib -v --features blas
-cargo miri test -p blas-mock-tests -v
-cargo miri test -p blas-tests -v --features blas-tests/openblas-system
-cargo miri test -p numeric-tests -v --features numeric-tests/test_blas
+cargo miri test -v -p ndarray -p ndarray-rand --lib
 
 # Examples
 cargo miri test --examples
