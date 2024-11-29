44980b5e7b276769a1eb6bf7c16374cedf1e47c5
Adam Kern <akern40@gmail.com>
2024-11-29 17:23:18 -0500

    Changes to BLIS for testing MSRV, since openblas is currently MSRV 1.67 and incompatible with ndarray's MSRV of 1.64

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -23,8 +23,16 @@ cargo test -v -p ndarray -p ndarray-rand --release --features "$FEATURES" $QC_FE
 # BLAS tests
 cargo test -p ndarray --lib -v --features blas
 cargo test -p blas-mock-tests -v
-cargo test -p blas-tests -v --features blas-tests/openblas-system
-cargo test -p numeric-tests -v --features numeric-tests/test_blas
+if [ "$CHANNEL" == "1.64.0" ]; then
+    cargo test -p blas-tests -v --features blas-tests/blis-system
+else
+    cargo test -p blas-tests -v --features blas-tests/openblas-system
+fi
+if [ "$CHANNEL" == "1.64.0" ]; then
+    cargo test -p numeric-tests -v --features numeric-tests/test_blis
+else
+    cargo test -p numeric-tests -v --features numeric-tests/test_blas
+fi
 
 # Examples
 cargo test --examples
