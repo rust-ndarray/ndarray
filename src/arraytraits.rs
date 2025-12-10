436e15c47665e832ff7e2a9ecdeeaca0d5bf028f
Jean Commère <jcommere@gaumut.com>
2025-12-10 15:03:10 +0100

    Implement Send for ArrayRef

--- src/arraytraits.rs
+++ src/arraytraits.rs
@@ -435,6 +435,8 @@ where
 
 unsafe impl<A, D> Sync for ArrayRef<A, D> where A: Sync {}
 
+unsafe impl<A, D> Send for ArrayRef<A, D> where A: Send {}
+
 #[cfg(feature = "serde")]
 #[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
 // Use version number so we can add a packed format later.
